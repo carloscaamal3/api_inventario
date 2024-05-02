76affe4bce475d177f02a82dc995ecbab1048828
DESKTOP-KLGM1HL\Carlos Caamal Can <carloscaamal3@gmail.com>
2024-05-01 18:48:26 -0600

    change login local

--- v1/datos/login_mysql.php
+++ v1/datos/login_mysql.php
@@ -14,10 +14,10 @@ define("USUARIO", "root"); // define nombre del usuario
 define("CONTRASENA", "rgala75"); // define constraseña
 */
 
-define("NOMBRE_HOST", "70.35.201.229:33306"); // define nombre del host
-define("BASE_DE_DATOS", "ivey_sicp"); // define nombre de la base de modelos
-define("USUARIO", "admin"); // define nombre del usuario
-define("CONTRASENA", "WCxaH82XOT3Y"); // define constraseÃ±a*/
+define("NOMBRE_HOST", "localhost"); // define nombre del host
+define("BASE_DE_DATOS", "sis_inventario"); // define nombre de la base de modelos
+define("USUARIO", "root"); // define nombre del usuario
+define("CONTRASENA", ""); // define constraseÃ±a*/
 
 /*
 define("NOMBRE_HOST", "70.35.201.229:33306"); // define nombre del host
